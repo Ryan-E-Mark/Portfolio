dbfcb28dbea27fda7e4f5cf4deff1c02050ca858
Ryan Mark <rymark907@gmail.com>
2022-01-28 14:09:28 -0800

    started navbar

---
+++ src/components/navbar.js
@@ -0,0 +1,18 @@
+import react from "react";
+
+const NavBar = () => {
+    return (
+        <section>
+            <div>
+
+            </div>
+            <div>
+                <a href="#about">About</a> 
+                <a href="#work">Work</a> 
+                <a href="#contact">Contact</a> 
+            </div>
+        </section>
+    )
+}
+
+export default NavBar;
