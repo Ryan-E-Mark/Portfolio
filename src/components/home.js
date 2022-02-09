7d6b0f1dd84702e7121f57c48df5bf8732fc6242
Ryan Mark <rymark907@gmail.com>
2022-02-09 13:16:30 -0800

    added a toggle for about me section

--- src/components/home.js
+++ src/components/home.js
@@ -1,13 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 import "../App.css";
 
 const Home = () => {
+    const [aboutMe, setAboutMe] = useState(false);
+
+    const handleClick = () => {
+        setAboutMe(!aboutMe);
+    }
+
+    console.log(aboutMe);
+
     return (
         <div className="home-page">
             <div>
                 <h1>Hi, I'm Ryan</h1>
                 <p>Full-Stack Developer</p>
-                <p>Something about me, what I like, do, who I am.</p>
+                <h3>About Me</h3>
+                <button onClick={handleClick}>+</button>
+                { aboutMe === true ? <p>About me paragraph</p> : <div></div>}
             </div>
         </div>
     )
